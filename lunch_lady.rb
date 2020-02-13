d3fccbb38e086f14c596dded03f4f675acd8e1e7
Seth Wheeler <wheeler.seth@gmail.com>
2020-02-13 14:03:30 -0700

    first commit

---
+++ lunch_lady.rb
@@ -0,0 +1,20 @@
+
+class User
+  attr_reader :name, :age
+  def initialize(name,money)
+    @name = name
+    @money = money
+  end
+  def say_hello
+    puts "Hi I am #{@name}"
+  end
+end
+
+john = User.new('John', 32)
+p john.age
+p john.say_hello
+
+# THESE ARE THE SAME, but you want to use class
+
+john1 = {name: 'John', age:32}
+p "say hello #{john1[:age]}"
